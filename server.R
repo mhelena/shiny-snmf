4583036addb91c04a4bdd48c6a7fba1ec67775c0
Helena Martins <ola.helenamartins@gmail.com>
2017-05-08 21:04:17 +0200

    First version snmf with k range defined by the user

--- server.R
+++ server.R
@@ -1,23 +1,57 @@
+library(shiny)
+source("https://bioconductor.org/biocLite.R")
+biocLite("LEA")
+library(LEA)
+
 function(input, output, session) {
 
   # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    iris[, c(input$xcol, input$ycol)]
-  })
+  #selectedData <- reactive({
+  #  iris[, c(input$xcol, input$ycol)]
+  #})
 
-  clusters <- reactive({
-    kmeans(selectedData(), input$clusters)
+  #clusters <- reactive({
+  #  kmeans(selectedData(), input$clusters)
+  #})
+  
+  output$text1 <- renderText({ 
+    paste("You have chosen a K range that goes from",
+          input$k[1], "to", input$k[2])
   })
-
+  
   output$plot1 <- renderPlot({
-    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-      "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+    #palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+    #  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
-    par(mar = c(5.1, 4.1, 0, 1))
-    plot(selectedData(),
-         col = clusters()$cluster,
-         pch = 20, cex = 3)
-    points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+    #par(mar = c(5.1, 4.1, 0, 1))
+    #plot(selectedData(),
+    #     col = clusters()$cluster,
+    #     pch = 20, cex = 3)
+    #points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+    
+    inFile <- input$genoFile
+    
+    if (is.null(inFile))
+      return(NULL)
+    
+    ext <- tools::file_ext(inFile$name)
+    file.rename(inFile$datapath,
+                paste(inFile$datapath, ext, sep="."))
+    fileLocation <- paste(inFile$datapath, ext, sep=".")
+    print(paste("The location: ", fileLocation[1]))
+    #return(NULL)
+    
+    krange <- c( input$k[1] : input$k[2])
+    
+    snmf.object = snmf(fileLocation,
+                       K=krange,
+                       ploidy = 2,
+                       repetitions = 1,
+                       entropy = TRUE,
+                       project = "new")
+    
+    #return(NULL)
+    plot(snmf.object)
   })
 
 }
--- ui.R
+++ ui.R
@@ -1,14 +1,27 @@
+library(shiny)
+
 pageWithSidebar(
   
   headerPanel('SNMF'),
   
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(iris)),
-    selectInput('ycol', 'Y Variable', names(iris), selected=names(iris)[[2]]),
-    numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
+    
+    fileInput("genoFile", "Choose geno File",
+              accept = c(".geno")),
+    
+    sliderInput("k",
+                "K:",
+                min = 1,
+                max = 20,
+                value = c(1,20))
+    
+    #selectInput('xcol', 'X Variable', names(iris)),
+    #selectInput('ycol', 'Y Variable', names(iris), selected=names(iris)[[2]]),
+    #numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
   ),
   
   mainPanel(
+    textOutput("text1"),
     plotOutput('plot1')
   )
   
